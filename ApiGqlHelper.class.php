214429b56268c3c5f5af5e0d65b08d0e1aff6af7
Megha BM <meghabm@sangoma.com>
2021-06-21 07:24:23 +0000

    added fwconsole chown and fwconsole r command to execute after upgrading all modules

--- ApiGqlHelper.class.php
+++ ApiGqlHelper.class.php
@@ -1,40 +1,59 @@
 
 <?php
-// vim: set ai ts=4 sw=4 ft=php:
 /**
  * This is the FreePBX Big Module Object.
  *
  * This is a very basic interface to the existing 'module_functions' class.
  *
  * License for all code of this FreePBX module can be found in the license file inside the module directory
- * Copyright 2006-2014 Schmooze Com Inc.
+ * Copyright 2013-2021 Sangoma Technologies Inc.
  */
-class ApiGqlHelper {
+class ApiGqlHelper extends \FreePBX_Helpers {
+	public function __construct($freepbx = null)
+	{
+		if ($freepbx == null) {
+			throw new \Exception("Not given a FreePBX Object");
+		}
+		$this->freepbx = $freepbx;
+	}
 
 	public function execGqlApi($args) {
 
-		$apiObj = \FreePBX::Api();
+		$apiObj = $this->freepbx->Api();
 
 		$module = strtolower($args[0]);
 		$action = strtolower($args[1]);
 		$track = $args[2];
 		$txnId = $args[3];
 
-		$bin = \FreePBX::Config()->get('AMPSBIN');
+		$bin =  $this->freepbx->Config()->get('AMPSBIN');
 		if($module == 'upgradeall'){
 			$action = $module;
 			$txnId = $args[2];
 			shell_exec($bin.'/fwconsole ma '.$action);
-		}else{
-			shell_exec($bin.'/fwconsole ma '.$action.' '.$module.' --'.$track);
+		} else {
+			shell_exec($bin . '/fwconsole ma ' . $action . ' ' . $module . ' --' . $track);
 		}
 
+		/**
+		 * Running below commands as root user
+		 */
+		$isSysAdminModuleInstalled = 0;
+		if ($this->freepbx->Modules->checkStatus("sysadmin")) {
+			$isSysAdminModuleInstalled = 1;
+			 $this->freepbx->Sysadmin()->ApiHooks()->runModuleSystemHook('api', 'fwconsole-commands', array('chown', $txnId));
+			 $this->freepbx->Sysadmin()->ApiHooks()->runModuleSystemHook('api', 'fwconsole-commands', array('reload', $txnId));
+		}
+	
 		$result = shell_exec($bin."/fwconsole ma list|grep ".$module ."|awk '{print $5 $6}'");
 
 		$reason = '';
 		$enabled = array('enable','install','upgrade');
 
-		if(in_array($action,$enabled) && $result ="|Enabled"){
+		if(!$isSysAdminModuleInstalled){
+			$status = "Failed";
+			$reason = "Sysadmin module has not been installed, hence failed to run the hook";
+		}else if(in_array($action,$enabled) && $result ="|Enabled"){
 			$status = "Executed";
 		}else if($action == "disable" && $result ="|Disabled"){
 			$status = "Executed";
