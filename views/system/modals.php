619b3f832214a1dbc5639888212324fb9d75b0ac
Franck Danard <fdanard@sangoma.com>
2021-06-29 16:53:19 +0200

    FREEI-3539 Add new API port to URLs to Client Credentials summary

--- views/system/modals.php
+++ views/system/modals.php
@@ -50,63 +50,171 @@
 		</div>
 	</div>
 </div>
+<!--- Modal api-info ---> 
 <div class="modal fade" tabindex="-1" role="dialog" id="api-info">
-	<div class="modal-dialog" role="document">
+	<div class="modal-dialog modal-lg" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h4 class="modal-title"><?php echo _("Client Credentials")?></h4>
 			</div>
 			<div class="modal-body">
-				<strong><?php echo ('Token URL')?></strong>:<span><?php echo $url?>/api/api/token</span><br/>
-				<strong><?php echo ('Authorization URL')?></strong>:<span><?php echo $url?>/api/api/authorize</span><br/>
-				<strong><?php echo ('GraphQL URL')?></strong>:<span><?php echo $url?>/api/api/gql</span><br/>
-				<strong><?php echo ('Rest URL')?></strong>:<span><?php echo $url?>/api/api/rest</span><br/>
-				<strong><?php echo ('Client ID')?></strong>:<span class="client_id"></span><br/>
-				<strong><?php echo ('Allowed Scopes')?></strong>:<span class="allowed_scopes"></span><br/>
-				<div class="client_secret_container">
-					<div class='alert alert-info'><?php echo _("Please copy your secret as you will not be able to retrieve it later")?></div>
-					<strong><?php echo ('Client Secret')?></strong>:<span class="client_secret"></span>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-12">					
+							<div class="col-md-2">
+							</div>
+							<div class="col-md-8 text-center button-filter" style="display:none">
+								<div id="api-filter" class="btn-group" role="group">
+									<button class="btn btn-secondary api-select" type="button" style="display:none"></button>			 
+									<button class="btn btn-secondary api-select left-btn-group" id="HTTP" type="button">HTTP</button> 
+									<button class="btn btn-secondary api-select" id="HTTPS" type="button">HTTPS</button> 
+									<button class="btn btn-secondary api-select" id="ADMINPorts"  type="button"><?php echo _("Admin Ports")?></button> 
+									<button class="btn btn-secondary api-select right-btn-group" id="APIPorts" type="button"><?php echo _("API Ports")?></button>
+									<button class="btn btn-secondary api-select" type="button" style="display:none"></button>
+								</div>
+							</div>
+							<div class="col-md-2">
+							</div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class="col-md-12">
+					<div class="row">
+						<div class="list-group">
+							<a href="#" class="list-group-item list-group-item-action active"><strong><?php echo _("URLs")?></strong></a>
+							<div class="list-group-item">
+								<div class="container-fluid" >
+									<div class="row no-gutter url-list">
+										<div class="col-md-2 text-right">
+											<strong><span><?php echo ('Token URL')?>:</span></strong>&nbsp;
+										</div>
+										<div class="col-md-10 text-left">
+											<span class="url-token"><span class="url"><?php echo $url?></span>/api/api/token</span> <i class="fa fa-clipboard" id="url-token" title="<?php echo _("Copy to clipboard")?>"></i>
+										</div>
+									</div>
+									<div class="row no-gutter url-list">
+										<div class="col-md-2 text-right">
+											<strong><?php echo ('Authori. URL')?>:<span></strong>&nbsp;
+										</div>
+										<div class="col-md-10 text-left">
+											<span class="url-authorize"><span class="url"><?php echo $url?></span>/api/api/authorize</span> <i class="fa fa-clipboard" id="url-authorize" title="<?php echo _("Copy to clipboard")?>"></i>
+										</div>
+									</div>
+									<div class="row no-gutter url-list">
+										<div class="col-md-2 text-right ">
+											<strong><?php echo ('GraphQL URL')?>:<span></strong>&nbsp;
+										</div>
+										<div class="col-md-10 text-left">
+											<span class="url-gql"><span class="url"><?php echo $url?></span>/api/api/gql</span> <i class="fa fa-clipboard" id="url-gql" title="<?php echo _("Copy to clipboard")?>"></i>
+										</div>
+									</div>
+									<div class="row no-gutter url-list">
+										<div class="col-md-2 text-right">
+											<strong><?php echo ('Rest URL')?>:<span></strong>&nbsp;
+										</div>
+										<div class="col-md-10 text-left">
+											<span class="url-rest"><span class="url"><?php echo $url?></span>/api/api/rest</span> <i class="fa fa-clipboard" id="url-rest" title="<?php echo _("Copy to clipboard")?>"></i>
+										</div>
+									</div>
+									<div class="row no-gutter proto-disabled" style="display:none">
+										<div class="col-md-12 text-center">
+											<h4><?php echo _("Protocol Disabled") ?></h4>
+										</div>
+									</div>
+								</div>
+							</div>
+							<a href="#" class="list-group-item list-group-item-action active"><strong><?php echo ('Client ID')?></strong></a>
+							<div class="list-group-item">							
+								<span class="client_id"></span> <i class="fa fa-clipboard" id="client_id" title="<?php echo _("Copy to clipboard")?>"></i>
+							</div>
+							<a href="#" class="list-group-item list-group-item-action active"><strong><?php echo ('Allowed Scopes')?></strong></a>
+							<div class="list-group-item">							
+								<span class="allowed_scopes"></span> <i class="fa fa-clipboard" id="allowed_scopes" title="<?php echo _("Copy to clipboard")?>"></i>
+							</div>
+							<span class="client_secret_container">
+								<a href="#" class="list-group-item list-group-item-action active"><?php echo "<b>"._('Client Secret') ."</b> <i>"._('(Copy your secret as you will not be able to retrieve it later).')."</i>" ?></a>
+								<div class="list-group-item">						
+									<span class="client_secret"></span>  <i class="fa fa-clipboard" id="client_secret" title="<?php echo _("Copy to clipboard")?>"></i>
+								</div>
+							</span>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" id="api-application-regenerate"><?php echo _('Regenerate Credentials')?></button>
 				<button type="button" class="btn btn-default" data-dismiss="modal"><?php echo _('Close')?></button>
+				<input type="text" value="" id="copytmp">
+				<input type="hidden" id="restapi" value="<?php echo !empty($data_api["API"]["HTTP"]) ? $data_api["API"]["HTTP"] : ""; ?>">
+				<input type="hidden" id="sslrestapi" value="<?php echo !empty($data_api["API"]["HTTPS"]) ? $data_api["API"]["HTTPS"] : ""; ?>">
+				<input type="hidden" id="acp" value="<?php echo !empty($data_api["ACP"]["HTTP"]) ? $data_api["ACP"]["HTTP"] : ""; ?>">
+				<input type="hidden" id="sslacp" value="<?php echo !empty($data_api["ACP"]["HTTPS"]) ? $data_api["ACP"]["HTTPS"] : ""; ?>">
+				<input type="hidden" id="fqdn" value="<?php echo str_replace(array("http://", "https://", "/admin"),array("","",""), $url)?>">
+				<input type="hidden" id="sysadmin" value="<?php echo $data_api["sa"] ;?>">
 			</div>
 		</div>
 	</div>
 </div>
+<!--- AND Modal api-info ---> 
+
 <div class="modal fade" id="urlModal" tabindex="-1" role="dialog" aria-labelledby="urlModalLabel">
-	<div class="modal-dialog" role="document">
+	<div class="modal-dialog modal-lg" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h4 class="modal-title" id="myModalLabel"><?php echo _("API URLs")?></h4>
 			</div>
 			<div class="modal-body">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-12">					
+							<div class="col-md-2">
+							</div>
+							<div class="col-md-8 text-center button-filter" style="display:none">
+								<div id="api-filter" class="btn-group" role="group">
+									<button class="btn btn-secondary api-select-um" type="button" style="display:none"></button>			 
+									<button class="btn btn-secondary api-select-um left-btn-group" id="HTTPum" type="button">HTTP</button> 
+									<button class="btn btn-secondary api-select-um" id="HTTPSum" type="button">HTTPS</button> 
+									<button class="btn btn-secondary api-select-um" id="ADMINPortsum"  type="button"><?php echo _("Admin Ports")?></button> 
+									<button class="btn btn-secondary api-select-um right-btn-group" id="APIPortsum" type="button"><?php echo _("API Ports")?></button>
+									<button class="btn btn-secondary api-select-um" type="button" style="display:none"></button>
+								</div>
+							</div>
+							<div class="col-md-2">
+							</div>
+						</div>
+					</div>
+				</div>
+				<br>
 				<div class="panel panel-default">
-					<div class="panel-heading"><?php echo sprintf(_("%s URLs"),"OAuth 2.0")?></div>
+					<div class="panel-heading active"><?php echo sprintf(_("%s URLs"),"OAuth 2.0")?></div>
 					<div class="panel-body">
-						<h5>Authorize: <?php echo $url?>/api/api/authorize</h5>
-						<h5>Token: <?php echo $url?>/api/api/token</h5>
-						<h5>Resource: <?php echo $url?>/api/api/resource</h5>
+						<h5 class="url-list">Authorize: <span class="url-authorize-um"><span class="url"><?php echo $url?></span>/api/api/authorize</span> <i class="fa fa-clipboard clipboard-um" id="url-authorize-um" title="<?php echo _("Copy to clipboard")?>"></i></h5>
+						<h5 class="url-list">Token: <span class="url-token-um"><span class="url"><?php echo $url?></span>/api/api/token</span> <i class="fa fa-clipboard clipboard-um" id="url-token-um" title="<?php echo _("Copy to clipboard")?>"></i></h5>
+						<h5 class="url-list">Resource: <span class="url-resource-um"><span class="url"><?php echo $url?></span>/api/api/resource</span> <i class="fa fa-clipboard clipboard-um" id="url-resource-um" title="<?php echo _("Copy to clipboard")?>"></i></h5>
+						<h5 class="proto-disabled-um" style="display:none"><?php echo _("Protocol Disabled") ?></h5>
 					</div>
 				</div>
 				<div class="panel panel-default">
-					<div class="panel-heading"><?php echo sprintf(_("%s URLs"),"GraphQL")?></div>
+					<div class="panel-heading active"><?php echo sprintf(_("%s URLs"),"GraphQL")?></div>
 					<div class="panel-body">
-						<h5>API: <?php echo $url?>/api/api/gql</h5>
+						<h5 class="url-list">API: <span class="url-gql-um"><span class="url"><?php echo $url?></span>/api/api/gql</span>  <i class="fa fa-clipboard clipboard-um" id="url-gql-um" title="<?php echo _("Copy to clipboard")?>"></i></h5>
+						<h5 class="proto-disabled-um" style="display:none"><?php echo _("Protocol Disabled") ?></h5>
 					</div>
 				</div>
 				<div class="panel panel-default">
-					<div class="panel-heading"><?php echo sprintf(_("%s URLs"),"RESTful")?></div>
+					<div class="panel-heading active"><?php echo sprintf(_("%s URLs"),"RESTful")?></div>
 					<div class="panel-body">
-						<h5>API: <?php echo $url?>/api/api/rest</h5>
+						<h5 class="url-list">API: <span class="url-restapi-um"><span class="url"><?php echo $url?></span>/api/api/rest</span>  <i class="fa fa-clipboard clipboard-um" id="url-restapi-um" title="<?php echo _("Copy to clipboard")?>"></i></h5>
+						<h5 class="proto-disabled-um" style="display:none"><?php echo _("Protocol Disabled") ?></h5>
 					</div>
 				</div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<input type="text" value="" id="copytmpum" style="color: white; border-color: white; font-size: 0px;">
 			</div>
 		</div>
 	</div>
