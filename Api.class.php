e6a87373f60d4503eb491246ff36ce9c33f3af56
Prashob K <prashobk@sangoma.com>
2023-05-15 12:49:04 +0530

    FREEI-65 prevent post authentication remote command execution in generatedocs method

--- Api.class.php
+++ Api.class.php
@@ -177,8 +177,8 @@ class Api extends \FreePBX_Helpers implements \BMO {
 				return ["status" => true, "token" => $token];
 			break;
 			case "generatedocs":
-				$this->generateDocumentation($_POST['scopes'], $_POST['host']);
-				return ["status" => true];
+				$status = $this->generateDocumentation($_POST['scopes'], $_POST['host']);
+				return ["status" => $status];
 			break;
 			case "getJSTreeScopes":
 				return $this->getJSTreeScopes();
@@ -461,6 +461,11 @@ class Api extends \FreePBX_Helpers implements \BMO {
 	}
 
 	public function generateDocumentation($scope, $host='http://localhost') {
+		$accessToken = $this->getDeveloperAccessToken($scope, $host);
+		if (!preg_match('/^[a-zA-Z0-9\-_.]+$/', $accessToken)) {
+			return false;
+		}
+
 		$ht = file_get_contents(__DIR__."/docs.htaccess");
 
 		$ht = str_replace('%ipaddress%',$_SERVER['REMOTE_ADDR'],$ht);
@@ -468,10 +473,11 @@ class Api extends \FreePBX_Helpers implements \BMO {
 		$process = new Process('rm -Rf '.__DIR__.'/docs');
 		$process->mustRun();
 
-		$process = new Process('NODE_TLS_REJECT_UNAUTHORIZED=0 node '.__DIR__.'/node/index.js -e '.$host.'/admin/api/api/gql -o '.__DIR__.'/docs -x "Authorization: Bearer '.$this->getDeveloperAccessToken($scope, $host).'"');
+		$process = new Process('NODE_TLS_REJECT_UNAUTHORIZED=0 node '.__DIR__.'/node/index.js -e '.$host.'/admin/api/api/gql -o '.__DIR__.'/docs -x "Authorization: Bearer '.$accessToken.'"');
 		$process->mustRun();
 
 		file_put_contents(__DIR__."/docs/.htaccess",$ht);
+		return true;
 	}
 
 	public function setTransactionStatus($transactionId,$status,$failureReason) {
