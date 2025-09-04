59fd461275cdf1300a4d7254bcfbf61330399637
Philip Joseph <jphilip@sangoma.com>
2025-09-04 12:17:33 +0000

    key permssion to 0600

--- Api.class.php
+++ Api.class.php
@@ -125,8 +125,9 @@ class Api extends \FreePBX_Helpers implements \BMO {
 		
 		$this->freepbx->PKCS->generateKey($this->oauthKey);
 		$this->freepbx->PKCS->extractPublicKey($this->oauthKey);
-
 		if(isset($keyfile) && file_exists($keyfile)) {
+			chmod($keyFile, 0600);
+			chmod($pubkeyfile, 0600);
 			$noKeyUpdateNotify = $this->getConfig('no-key-update-notifiy');
 			if (!$noKeyUpdateNotify) {
 				$nt = \notifications::create();
