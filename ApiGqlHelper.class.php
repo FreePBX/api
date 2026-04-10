9c8a2f4a312f5538bb52b2dc0156754cb06fd766
Philip Joseph <jphilip@sangoma.com>
2026-04-10 02:08:35 -0400

    Has mismatch issue

--- ApiGqlHelper.class.php
+++ ApiGqlHelper.class.php
@@ -27,16 +27,15 @@ class ApiGqlHelper extends \FreePBX_Helpers {
 		$txnId = $args[3];
 
 		$bin =  $this->freepbx->Config()->get('AMPSBIN');
-		$fwconsole = escapeshellarg($bin . '/fwconsole');
 		if($module == 'upgradeall'){
 			$action = $module;
 			$txnId = $args[2];
-			shell_exec($fwconsole . ' ma ' . escapeshellarg($action));
+			shell_exec($bin.'/fwconsole ma '.$action);
 		} else {
-			shell_exec($fwconsole . ' ma ' . escapeshellarg($action) . ' ' . escapeshellarg($module) . ' --' . escapeshellarg((string) $track));
+			shell_exec($bin . '/fwconsole ma ' . $action . ' ' . $module . ' --' . $track);
 		}
 	
-		$result = shell_exec($fwconsole . ' ma list|grep ' . escapeshellarg($module) . "|awk '{print $5 $6}'");
+		$result = shell_exec($bin."/fwconsole ma list|grep ".$module ."|awk '{print $5 $6}'");
 
 		$reason = '';
 		$enabled = ['enable', 'install', 'upgrade'];
